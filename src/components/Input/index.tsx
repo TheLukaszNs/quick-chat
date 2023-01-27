ba472e9e4193f898f0e6b506dd6c84ddc49dacbd
Kacper Grabiec <grabiec.kacper01@gmail.com>
2023-01-27 20:40:59 +0100

    reformat Input/index.tsx file

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -2,18 +2,22 @@ import { Component, InputHTMLAttributes } from "react";
 import { IconType } from "react-icons/lib/esm/iconBase";
 
 type Props = {
-    inputType: InputHTMLAttributes<HTMLInputElement>["type"];
-    placeholder: InputHTMLAttributes<HTMLInputElement>["placeholder"];
-    icon: IconType;
-}
+  inputType: InputHTMLAttributes<HTMLInputElement>["type"];
+  placeholder: InputHTMLAttributes<HTMLInputElement>["placeholder"];
+  icon: IconType;
+};
 
-const Input = ({inputType, placeholder, icon: Icon}: Props) => {
-    return (
-        <div className="w-80 h-16 bg-slate-600 rounded-2xl my-4 flex flex-row justify-between items-center">
-            <input type={inputType} className="bg-inherit rounded-2xl text-slate-50 font-bold ml-6 outline-none" placeholder={placeholder}/>
-            <div className="text-slate-50 font-bold mr-6">{<Icon/>}</div>
-        </div>
-    );
-}
+const Input = ({ inputType, placeholder, icon: Icon }: Props) => {
+  return (
+    <div className="my-4 flex h-16 w-80 flex-row items-center justify-between rounded-2xl bg-slate-600">
+      <input
+        type={inputType}
+        className="ml-6 rounded-2xl bg-inherit font-bold text-slate-50 outline-none"
+        placeholder={placeholder}
+      />
+      <div className="mr-6 font-bold text-slate-50">{<Icon />}</div>
+    </div>
+  );
+};
 
 export default Input;
