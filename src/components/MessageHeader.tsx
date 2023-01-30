ad8a63865e59fd24adf2c7d5a4f3007aa04f3994
Wojciech Brożek <wobrozek@gmail.com>
2023-01-30 14:32:36 +0100

    add files

--- src/components/MessageHeader.tsx
+++ src/components/MessageHeader.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Link from "next/link";
 import { BsFillPersonPlusFill } from "react-icons/bs";
 import { BiMessageRoundedAdd } from "react-icons/bi";
+import Image from "next/image";
 
 interface HeaderProps {
   name: string;
@@ -15,11 +16,11 @@ export const MessageHeader = ({ name, photo, isRoom, active }: HeaderProps) => {
     <nav className="sticky top-0 left-0 right-0 mb-5 flex items-center justify-between bg-slate-800 p-5">
       <div className="flex items-center">
         <div>
-          <img
+          <Image
             src={`data:image/jpeg;base64,${photo}`}
             className="h-10 w-10 rounded-full "
             alt=""
-          ></img>
+          ></Image>
         </div>
         <div className="flex-col px-5">
           <div>{name}</div>
--- src/components/MessageUser.tsx
+++ src/components/MessageUser.tsx
@@ -1,5 +1,6 @@
 import { User } from "@prisma/client";
 import React from "react";
+import Image from "next/image"
 
 interface MessageProps {
   message: string;
@@ -11,11 +12,11 @@ const MessageUser = ({ message, user, isDm }: MessageProps) => {
   return (
     <div className="m-auto mb-5 flex w-4/5">
       <div>
-        <img
+        <Image
           src={`data:image/jpeg;base64,${user.image}`}
           className="h-10 w-10 rounded-full"
           alt=""
-        ></img>
+        ></Image>
       </div>
       <div className="flex-col">
         {isDm && <div>@{user.name}</div>}
