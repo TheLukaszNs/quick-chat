12aeac5d81c0cec6b91dbb205119c8ec9345df50
Kacper Grabiec <grabiec.kacper01@gmail.com>
2023-01-31 18:41:27 +0100

    add  UserListItem component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,19 +1,20 @@
+import { ComponentPropsWithoutRef } from "react";
 import { Component, InputHTMLAttributes } from "react";
 import { IconType } from "react-icons/lib/esm/iconBase";
 
-type Props = {
+type Props = ComponentPropsWithoutRef<"input"> & {
   inputType: InputHTMLAttributes<HTMLInputElement>["type"];
-  placeholder: string;
   icon: IconType;
 };
 
-const Input = ({ inputType, placeholder, icon: Icon }: Props) => {
+const Input = ({ inputType, placeholder, icon: Icon, ...props }: Props) => {
   return (
     <div className="my-4 flex h-16 w-80 flex-row items-center justify-between rounded-2xl bg-slate-600">
       <input
         type={inputType}
         className="ml-6 rounded-2xl bg-inherit font-bold text-slate-50 outline-none"
         placeholder={placeholder}
+        {...props}
       />
       <div className="mr-6 font-bold text-slate-50">
         <Icon />
