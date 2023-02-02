07796c66ffc9e53b8b152af0e98f62aadd74f762
Wojciech Brożek <wobrozek@gmail.com>
2023-02-02 22:45:26 +0100

    add calback to input for onclic message

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,23 +1,47 @@
-import { ComponentPropsWithoutRef } from "react";
+import { ComponentPropsWithoutRef, useRef, useState } from "react";
 import { Component, InputHTMLAttributes } from "react";
 import { IconType } from "react-icons/lib/esm/iconBase";
+import { string } from "zod";
 
 type Props = ComponentPropsWithoutRef<"input"> & {
   inputType: InputHTMLAttributes<HTMLInputElement>["type"];
   icon: IconType;
+  clickIconFunction?: () => void;
 };
+const Input = ({
+  inputType,
+  placeholder,
+  icon: Icon,
+  clickIconFunction,
+  ...props
+}: Props) => {
+  const [value, setValue] = useState("");
 
-const Input = ({ inputType, placeholder, icon: Icon, ...props }: Props) => {
+  // functions that is needed to manipulate input for example to set input to empty after sending message
+  const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
+  const changeValue = (newValue: string): void => {
+    setValue(newValue);
+  };
+
   return (
     <div className="my-4 flex h-16 w-80 flex-row items-center justify-between rounded-2xl bg-slate-600">
       <input
         type={inputType}
         className="ml-6 rounded-2xl bg-inherit font-bold text-slate-50 outline-none"
         placeholder={placeholder}
+        value={value}
         {...props}
+        onChange={handleChange}
       />
       <div className="mr-6 font-bold text-slate-50">
-        <Icon />
+        <Icon
+          onClick={(e) => {
+            clickIconFunction(e, value, changeValue);
+          }}
+        />
       </div>
     </div>
   );
