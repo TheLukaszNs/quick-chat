05da3731a1002264f5c3380ec2bc97472dbdac3c
Kacper Grabiec <grabiec.kacper01@gmail.com>
2023-02-03 21:37:42 +0100

    fix room page

--- src/pages/room/[id].tsx
+++ src/pages/room/[id].tsx
@@ -1,27 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import MessageHeader from "../../components/MessageHeader";
 import MessageUser from "../../components/MessageUser";
-import Input from "../../components/Input";
+import InputMessage from "../../components/InputMessage";
 import { AiOutlineSend } from "react-icons/ai";
 
 const Room = () => {
-  return (
-    <main className="flex h-screen w-screen flex-col items-center bg-slate-900">
-      <div className="overflow-hidden">
-        <MessageHeader
-          name="Wiesiu Sus"
-          photo={""}
-          active={1}
-          isRoom={false}
-        ></MessageHeader>
-      </div>
+  const [messages, setMessages] = useState([
+    { id: 0, message: "blue is impostor", user: User },
+    { id: 1, message: "I saw blue vent", user: User },
+    { id: 2, message: "vote for blue", user: User },
+  ]);
 
-      <div className="absolute bottom-5">
-        <Input
-          inputType="search"
-          icon={AiOutlineSend}
-          placeholder="Wpisz Wiadomość"
-        ></Input>
+  const handleSend = (value: string): void => {
+    if (!value) return;
+    setMessages((prev) => [...prev, { id: 4, user: User, message: value }]);
+  };
+
+  return (
+    <main className="flex h-screen w-screen flex-col overflow-hidden bg-slate-900">
+      <MessageHeader
+        name="Wiesiu Sus"
+        photo={""}
+        active={1}
+        isRoom={false}
+      ></MessageHeader>
+      <div className="flex h-full max-h-[80%] flex-col justify-end bg-slate-900">
+        <div className="overflow-y-scroll">
+          {messages.map((element) => {
+            return (
+              <MessageUser
+                message={element.message}
+                user={element.user}
+                isDm={true}
+                key={element.id}
+              ></MessageUser>
+            );
+          })}
+        </div>
+
+        <div className="self-center">
+          <InputMessage
+            inputType="search"
+            icon={AiOutlineSend}
+            placeholder="wyslij wiadomosc"
+            handleSend={handleSend}
+          ></InputMessage>
+        </div>
       </div>
     </main>
   );
