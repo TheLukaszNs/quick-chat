7affe6a465610a664113eb52c764bfbdb561289d
Łukasz Myśliwiec <lmysliwiec10@gmail.com>
2023-02-03 21:04:06 +0100

    fix: more linting errors ✨

--- src/pages/server/index.tsx
+++ src/pages/server/index.tsx
@@ -1,13 +1,9 @@
 import Link from "next/link";
 import { MessageHeader } from "../../components/MessageHeader";
-import { useRouter } from "next/router";
-import { api } from "../../utils/api";
 
 const rooms: string[] = ["room 1", "room 2", "room 3", "room 4", "room 5"];
 
 const Server = () => {
-  const { query } = useRouter();
-
   return (
     <main className="flex h-screen w-screen flex-col bg-slate-900">
       <MessageHeader name="Sample Name" photo="" isRoom={true} active={10} />
