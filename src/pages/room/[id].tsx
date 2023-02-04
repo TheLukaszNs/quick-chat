ee3509250330516b2f1d5b488f9f8fb789fcafac
Łukasz Myśliwiec <lmysliwiec10@gmail.com>
2023-02-04 21:45:18 +0100

    feat: add ability to send messages ⚡

--- src/pages/room/[id].tsx
+++ src/pages/room/[id].tsx
@@ -1,39 +1,90 @@
-import React, { useState } from "react";
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
 import MessageHeader from "../../components/MessageHeader";
 import MessageUser from "../../components/MessageUser";
 import InputMessage from "../../components/InputMessage";
 import { AiOutlineSend } from "react-icons/ai";
 import type { Message } from "@prisma/client";
+import { useRouter } from "next/router";
+import { api } from "../../utils/api";
+import { useSession } from "next-auth/react";
+import { useIsInitialRender } from "../../hooks/useIsInitialRender";
 
 const Room = () => {
-  const [messages, setMessages] = useState<Message[]>([]);
+  const messagesContainerRef = useRef<HTMLDivElement>(null);
+  const isInitial = useIsInitialRender();
 
-  const handleSend = (value: string): void => {
-    if (!value) return;
-    setMessages([]);
-  };
+  const router = useRouter();
+  const { data: sessionData } = useSession();
+
+  const { id } = router.query;
+  const userId = sessionData?.user?.id;
+
+  const roomQuery = api.direct.getDirectRoom.useQuery(
+    {
+      directRoomId: id as string,
+    },
+    {
+      enabled: !!id,
+      onSettled: () => {
+        void new Promise((resolve) => {
+          setTimeout(() => {
+            messagesContainerRef.current?.scrollTo({
+              top: messagesContainerRef.current?.scrollHeight,
+              behavior: isInitial ? "auto" : "smooth",
+            });
+            resolve(true);
+          }, 100);
+        });
+      },
+    }
+  );
+
+  const newMessageMutation = api.direct.newMessage.useMutation();
+  api.direct.onMessage.useSubscription(
+    {
+      directRoomId: id as string,
+    },
+    {
+      enabled: !!id,
+      onData: () => {
+        void roomQuery.refetch();
+      },
+    }
+  );
+
+  if (roomQuery.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  const { data: room } = roomQuery;
+
+  if (!room) {
+    return <div>Room not found</div>;
+  }
+
+  const chatter = room.users.find((user) => user.id !== sessionData?.user?.id);
 
   return (
     <main className="flex h-screen w-screen flex-col overflow-hidden bg-slate-900">
       <MessageHeader
-        name="Wiesiu Sus"
-        photo={""}
+        name={chatter?.name ?? ""}
+        photo={chatter?.image ?? ""}
         active={1}
         isRoom={false}
-      ></MessageHeader>
+      />
       <div className="flex h-full max-h-[80%] flex-col justify-end bg-slate-900">
-        <div className="overflow-y-scroll">
-          {messages.map((element) => {
+        <div className="overflow-y-scroll" ref={messagesContainerRef}>
+          {room.messages.map((element) => {
             return (
               <MessageUser
                 message={element.content}
                 user={{
-                  name: "Wiesiu Sus",
-                  image: "",
+                  name: element.author.name,
+                  image: element.author.image,
                 }}
                 isDm={true}
                 key={element.id}
-              ></MessageUser>
+              />
             );
           })}
         </div>
@@ -43,7 +94,12 @@ const Room = () => {
             inputType="search"
             icon={AiOutlineSend}
             placeholder="wyslij wiadomosc"
-            handleSend={handleSend}
+            handleSend={(value) => {
+              void newMessageMutation.mutateAsync({
+                directRoomId: id as string,
+                text: value,
+              });
+            }}
           ></InputMessage>
         </div>
       </div>
