3ffa003fb3553cc81ba51a21c3797aa7e533bfc2
Kacper Grabiec <grabiec.kacper01@gmail.com>
2023-02-04 12:27:05 +0100

    add ServerListItem component

---
+++ src/components/ServerListItem/index.tsx
@@ -0,0 +1,11 @@
+import { ComponentPropsWithoutRef } from "react";
+
+type Props = ComponentPropsWithoutRef<"button"> & {
+  serverName: string;
+};
+
+const ServerListItem = ({ serverName, ...props }: Props) => {
+  return <button {...props}>{serverName}</button>;
+};
+
+export default ServerListItem;
